abdb3b73b461b38681777ebf6a62361ed34f538d
lrokke2 <lrokke2@huskers.unl.edu>
2023-08-28 15:24:39 -0500

    added my major to the output

--- hello.c
+++ hello.c
@@ -4,7 +4,8 @@
 int main(int argc, char **argv) {
 
   printf("Hello World, I'm Femi Odulate!\n");
-
   printf("My major is computer science.\n");
+  printf("Hello World, Lewis Rokke!\n");
+  printf("My major is also computer science.\n");
   return 0;
 }
