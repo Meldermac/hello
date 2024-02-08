c8cf216d53d069617fa07818c3464a2083daaca9
Meldermac <142739632+Meldermac@users.noreply.github.com>
2024-02-07 21:00:28 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,11 +1,90 @@
-#include <stdlib.h>
-#include <stdio.h>
+package unl.soc;
 
-int main(int argc, char **argv) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-  printf("Hello World, I'm Femi Odulate!\n");
-  printf("My major is computer science.\n");
-  printf("Hello World, Lewis Rokke!\n");
-  printf("My major is also computer science.\n");
-  return 0;
+/**
+ * Processes DNA data and counts the number of instances that a particular DNA
+ * subsequence appears.
+ *
+ */
+public class DnaAnalysis {
+
+	private static final String DNA_FILE = "data/H1N1nucleotide.txt";
+	private static final String DNA;
+	
+	static {
+		// load the contents of the file statically (when the class
+		// is loaded)
+		DNA = loadDnaFromFile();
+	}
+
+	/**
+	 * Loads a DNA string from the {@link #DNA_FILE} and returns it as a string with
+	 * all whitespace removed.
+	 * 
+	 * @return
+	 */
+	public static String loadDnaFromFile() {
+
+		Scanner s = null;
+		try {
+			s = new Scanner(new File(DNA_FILE));
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+
+		String dna = "";
+
+		while (s.hasNextLine()) {
+			dna += s.nextLine();
+		}
+		// close the scanner
+		s.close();
+		// strip all whitespace
+		dna = dna.replaceAll("\\s+", "");
+		return dna;
+	}
+
+	/**
+	 * Counts the number of times <code>subSequence</code> appears in the
+	 * {@link #DNA} string
+	 * 
+	 * @return
+	 */
+	public static int countSubsequences(String subSequence) {
+
+		int count = 0;
+		
+		int i = 0;
+		int subLength = subSequence.length();
+		
+		while(i + subLength < DNA.length() + 1) {
+			if(DNA.substring(i, i + subLength).equals(subSequence)) {
+				count++;
+			}
+			i += 1;
+		}
+		
+		return count;
+	}
+
+	public static void main(String args[]) {
+
+		String subSequence = null;
+		
+		if(args.length != 1) {
+			System.out.println("No command line arguements or incorrect number of command line arguements given.");
+			return;
+		} else {
+			subSequence = args[0];
+		}
+
+		int count = countSubsequences(subSequence);
+
+		System.out.println(subSequence + " appears " + count + " times");
+
+	}
+
 }
